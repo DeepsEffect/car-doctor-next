9b3c343ea470e3d6ec1086e8c60a517384f4611f
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-07-09 13:58:32 +0600

    basic navbar

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,11 +1,92 @@
-import React from 'react';
+"use client";
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
+import Image from "next/image";
 
-const Navbar = () => {
-    return (
-        <div>
-            navbar
-        </div>
-    );
-};
+export default function Nav() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-export default Navbar;
+  const menuItems = [
+    {
+      title: "Home",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Services",
+      path: "/services",
+    },
+    {
+      title: "Blog",
+      path: "/blog",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+  ];
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <Image src={"/assets/logo.svg"} alt="logo" width={70} height={100} />
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        {menuItems.map((menu) => (
+          <NavbarItem key={menu.title}>
+            <Link href={menu.path}>{menu.title}</Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Button color="primary" variant="bordered">
+            Appointment
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+        {/* mobile menu */}
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item.title}-${index}`}>
+            <Link
+              color={
+                index === 2
+                  ? "primary"
+                  : index === menuItems.length - 1
+                  ? "danger"
+                  : "foreground"
+              }
+              className="w-full"
+              href={item.path}
+              size="lg"
+            >
+              {item.title}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
